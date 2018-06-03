7f2dba71598c5ed96ceb50c7c10b74b729ab0e76
AdamAM <adamam@gmail.com>
2018-06-02 21:32:16 -0600

    Initial Commit

---
+++ SolarSystemObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SolarSystemObjC
+//
+//  Created by Adam on 02/06/2018.
+//  Copyright © 2018 Adam Moskovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SolarSystemObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SolarSystemObjC
+//
+//  Created by Adam on 02/06/2018.
+//  Copyright © 2018 Adam Moskovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
